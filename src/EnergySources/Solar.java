75d11debef8b6bb7a512ee66e7864b744d1b84cd
Reder <reder@mre.lap>
2020-01-19 13:03:28 +0100

    added solar class

--- src/EnergySources/Solar.java
+++ src/EnergySources/Solar.java
@@ -1,4 +1,37 @@
 package EnergySources;
 
 public class Solar {
+    double wattPeak;
+    double weatherCoefficient;
+
+    public Solar(double wattPeak, double weatherCoefficient) {
+        this.wattPeak = wattPeak;
+        this.weatherCoefficient = weatherCoefficient;
+    }
+
+    public double getWattPeak() {
+        return wattPeak;
+    }
+
+    public void setWattPeak(double wattPeak) {
+        this.wattPeak = wattPeak;
+    }
+
+    public double getWeatherCoefficient() {
+        return weatherCoefficient;
+    }
+
+    public void setWeatherCoefficient(double weatherCoefficient) {
+        this.weatherCoefficient = weatherCoefficient;
+    }
+
+    public double dailyOutput(){
+
+        return 0.0;
+    }
+
+    public double hourOutput(){
+
+        return 0.0;
+    }
 }
